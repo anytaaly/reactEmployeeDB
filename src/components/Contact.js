8339426de414d8db9b5fecb2563a7453d9ca4597
anytaaly <anytarali@gmail.com>
2019-01-21 11:10:09 -0600

    adding Delete button functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,18 +2,42 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
+
+  onShowClick = () => {
+    console.log("clicked");
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+
+  onDeleteClick = () => {
+    console.log("onShow Clicked");
+  };
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
         <h4>
           {" "}
-          {name} <i className="fas fa-plus"> </i>{" "}
+          {name}{" "}
+          <i
+            className="fas fa-sort-down"
+            onClick={this.onShowClick}
+            style={{ cursor: "pointer" }}
+          />
+          <i
+            className="fas fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+            onClick={this.onDeleteClick}
+          />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">{email}</li>
-          <li className="list-group-item"> {phone}</li>
-        </ul>
+        {this.state.showContactInfo && (
+          <ul className="list-group">
+            <li className="list-group-item">{email}</li>
+            <li className="list-group-item"> {phone}</li>
+          </ul>
+        )}
       </div>
     );
   }
