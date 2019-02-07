61bd1a45744a657e44a75b290ed3991d1c5631fb
anytaaly <anytarali@gmail.com>
2019-02-06 21:17:43 -0600

    Updates

---
+++ precache-manifest.a64ca505700c37b24a86405a6b15bcfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3d04bddb53785ee59b1",
+    "url": "/reactEmployeeDB/static/js/runtime~main.e3d04bdd.js"
+  },
+  {
+    "revision": "2a8a44cdc9b771cb5efb",
+    "url": "/reactEmployeeDB/static/js/main.2a8a44cd.chunk.js"
+  },
+  {
+    "revision": "a74513d01967d18a784d",
+    "url": "/reactEmployeeDB/static/js/1.a74513d0.chunk.js"
+  },
+  {
+    "revision": "2a8a44cdc9b771cb5efb",
+    "url": "/reactEmployeeDB/static/css/main.6e9e20bf.chunk.css"
+  },
+  {
+    "revision": "a74513d01967d18a784d",
+    "url": "/reactEmployeeDB/static/css/1.e808513b.chunk.css"
+  },
+  {
+    "revision": "09ff71e392873ef3cb4c93c1bc86e1fd",
+    "url": "/reactEmployeeDB/index.html"
+  }
+];
